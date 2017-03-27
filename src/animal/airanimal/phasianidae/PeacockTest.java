4c3ad0a7b0d1ae11affd72c5f1e35d91e25de16f
zackizf1204 <zackizf1204@gmail.com>
2017-03-27 23:26:58 +0700

    Add PeacockTest

--- src/animal/airanimal/phasianidae/PeacockTest.java
+++ src/animal/airanimal/phasianidae/PeacockTest.java
@@ -13,22 +13,37 @@ import static org.junit.jupiter.api.Assertions.*;
 class PeacockTest {
     @Test
     void getFoodNum() {
+        Peacock E = new Peacock();
+        int I = 15;
+        assertTrue(E.GetFoodNum()== I);
     }
 
     @Test
     void getRender() {
+        Peacock E = new Peacock();
+        char C = 'M';
+        assertTrue(E.GetRender()== C);
     }
 
     @Test
     void getSpecies() {
+        Peacock E = new Peacock();
+        String S = "Afropavo congensis";
+        assertTrue(E.GetSpecies()== S);
     }
 
     @Test
     void getFamili() {
+        Peacock E = new Peacock();
+        String S = "Phasianidae";
+        assertTrue(E.GetFamili()== S);
     }
 
     @Test
     void getIsiExperience() {
+        Peacock E = new Peacock();
+        String S = "Rainbow Bird";
+        assertTrue(E.GetIsiExperience()== S);
     }
 
 }
